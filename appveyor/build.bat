54537d83ef6ac76aaae06e4fe68ddcbd4bb9182c
M*C*O <mcofficer@gmx.de>
2019-06-11 21:16:40 +0200

    a bucket of fixes

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -2,11 +2,12 @@ REM Dependencies
 C:\Python37-x64\python -m pip install nuitka pyinstaller
 
 REM Nuitka Compilation
-C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
-7z a -tzip -mx9 -y artifacts\ESMB-win64-nuitka.zip .\ESMB.dist\*
+C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter --output-dir ESMB ESMB.py
+7z a -tzip -mx9 -y ESMB-win64-nuitka.zip .\ESMB\
 
 REM PyInstaller
 C:\Python37-x64\Scripts\pyinstaller -D -y -w ESMB.py
-7z a -tzip -mx9 -y artifacts\ESMB-win64-pyinstaller.zip .\ESMB.dist\*
-C:\Python37-x64\Scripts\pyinstaller -F -y -w ESMB.py
-cp dist\ESMB.exe artifacts\ESMB-win64-pyinstaller.exe
+7z a -tzip -mx9 -y ESMB-win64-pyinstaller.zip .\dist\ESMB
+
+C:\Python37-x64\Scripts\pyinstaller -F -y -w ESMB.py --dispath onefile.dist
+cp onefile.dist\ESMB ESMB-win64-pyinstaller.exe
--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,12 +1,14 @@
 # Dependencies
-python3 -m pip install nuitka pyinstaller
+pip3 install nuitka pyinstaller
 
 # Nuitka Compilation
 python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
-tar -czvf ESMB-amd64-nuitka.zip ./ESMB.dist/*
+mv ESMB.dist ESMB
+tar -czvf ESMB-amd64-nuitka.zip ESMB/
 
 # PyInstaller
 pyinstaller -D -y -w ESMB.py
-tar -czvf ESMB-amd64-pyinstaller.zip ./ESMB.dist/*
-pyinstaller -F -y -w ESMB.py
-cp dist/ESMB ESMB-amd64-pyinstaller
+tar -czvf ESMB-amd64-pyinstaller.zip dist/ESMB/
+
+pyinstaller -F -y -w ESMB.py --distpath onefile.dist
+cp onefile.dist/ESMB ESMB-amd64-pyinstaller
