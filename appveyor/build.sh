6f0db96f654e23ec9055ca68dea2f14e335da622
M*C*O <mcofficer@gmx.de>
2019-06-11 21:54:31 +0200

    tarballs instead of zips

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -5,12 +5,12 @@ pip3 install nuitka pyinstaller
 # Nuitka Compilation
 python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
 mv ESMB.dist ESMB
-tar -czvf ESMB-amd64-nuitka.zip ESMB/
+tar -czvf ESMB-amd64-nuitka.tar.gz ESMB/
 rm -rf ESMB
 
 # PyInstaller
 pyinstaller -D -y -w ESMB.py
-tar -czvf ESMB-amd64-pyinstaller.zip dist/ESMB/
+tar -czvf ESMB-amd64-pyinstaller.tar.gz dist/ESMB/
 rm -rf dist
 
 pyinstaller -F -y -w ESMB.py
