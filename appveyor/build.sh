6447c9784e74c10662bdc913e3c46cac0d60432b
M*C*O <mcofficer@gmx.de>
2019-06-11 20:57:34 +0200

    all slashes the wrong way

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -3,10 +3,10 @@ python3 -m pip install nuitka pyinstaller
 
 # Nuitka Compilation
 python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
-tar -czvf ESMB-amd64-nuitka.zip .\ESMB.dist\*
+tar -czvf ESMB-amd64-nuitka.zip ./ESMB.dist/*
 
 # PyInstaller
 pyinstaller -D -y -w ESMB.py
-tar -czvf ESMB-amd64-pyinstaller.zip .\ESMB.dist\*
+tar -czvf ESMB-amd64-pyinstaller.zip ./ESMB.dist/*
 pyinstaller -F -y -w ESMB.py
-cp dist\ESMB ESMB-amd64-pyinstaller
+cp dist/ESMB ESMB-amd64-pyinstaller
