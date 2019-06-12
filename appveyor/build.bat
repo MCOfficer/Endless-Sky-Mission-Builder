928929cfe2d601b3aeea2ba185a8bdba7fa8f4f4
M*C*O <mcofficer@gmx.de>
2019-06-12 13:14:38 +0200

    revert to x64 builds only

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -1,26 +1,17 @@
 REM Setup
-set PLATFORM=win64
-set PYTHON=C:\Python37-x64\python
-IF %PROCESSOR_ARCHITECTURE% == x86 (
-  IF NOT DEFINED PROCESSOR_ARCHITEW6432 (
-    set PLATFORM=win32
-    set PYTHON=C:\Python37\python
-  )
-)
-echo Running on %PLATFORM%
-%PYTHON% -m pip install nuitka pyinstaller
+C:\Python37-x64\python -m pip install nuitka pyinstaller
 
 REM Nuitka Compilation
-%PYTHON% -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
+C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
 mv ESMB.dist ESMB
-7z a -tzip -mx9 -y ESMB-%PLATFORM%-nuitka.zip .\ESMB\
+7z a -tzip -mx9 -y ESMB-win64-nuitka.zip .\ESMB\
 rd /S /Q ESMB
 
 REM PyInstaller
 C:\Python37-x64\Scripts\pyinstaller -D -y -w ESMB.py
-7z a -tzip -mx9 -y ESMB-%PLATFORM%-pyinstaller.zip .\dist\ESMB
+7z a -tzip -mx9 -y ESMB-win64-pyinstaller.zip .\dist\ESMB
 rd /S /Q dist
 
 C:\Python37-x64\Scripts\pyinstaller -F -y -w ESMB.py
-cp dist\ESMB ESMB-%PLATFORM%-pyinstaller.exe
+cp dist\ESMB ESMB-win64-pyinstaller.exe
 rd /S /Q dist
--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,20 +1,18 @@
 # Setup
-PLATFORM=$(uname -i)
-echo Running on $PLATFORM
 sudo apt-get update && sudo apt-get -y install python3-pip python3-tk
 pip3 install nuitka pyinstaller
 
 # Nuitka Compilation
 python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons ESMB.py
 mv ESMB.dist ESMB
-tar -czvf ESMB-ubuntu-$PLATFORM-nuitka.tar.gz ESMB/
+tar -czvf ESMB-ubuntu-amd64-nuitka.tar.gz ESMB/
 rm -rf ESMB
 
 # PyInstaller
 pyinstaller -D -y -w ESMB.py
-tar -czvf ESMB-ubuntu-$PLATFORM-pyinstaller.tar.gz dist/ESMB/
+tar -czvf ESMB-ubuntu-amd64-pyinstaller.tar.gz dist/ESMB/
 rm -rf dist
 
 pyinstaller -F -y -w ESMB.py
-cp dist/ESMB ESMB-ubuntu-$PLATFORM-pyinstaller
+cp dist/ESMB ESMB-ubuntu-amd64-pyinstaller
 rm -rf dist
