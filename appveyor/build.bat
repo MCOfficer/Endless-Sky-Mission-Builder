7b184d11a07ebc4854297479ae57e08ac805a7bf
M*C*O <mcofficer@gmx.de>
2019-06-12 12:37:07 +0200

    name artifacts depending on architecture

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -1,17 +1,23 @@
-REM Dependencies
+REM Setup
+Set _os_bitness=64
+IF %PROCESSOR_ARCHITECTURE% == x86 (
+  IF NOT DEFINED PROCESSOR_ARCHITEW6432 Set _os_bitness=32
+)
+set PLATFORM=win%_os_bitness%
+echo Running on %PLATFORM%
 C:\Python37-x64\python -m pip install nuitka pyinstaller
 
 REM Nuitka Compilation
 C:\Python37-x64\python -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
 mv ESMB.dist ESMB
-7z a -tzip -mx9 -y ESMB-win64-nuitka.zip .\ESMB\
+7z a -tzip -mx9 -y ESMB-%PLATFORM%-nuitka.zip .\ESMB\
 rd /S /Q ESMB
 
 REM PyInstaller
 C:\Python37-x64\Scripts\pyinstaller -D -y -w ESMB.py
-7z a -tzip -mx9 -y ESMB-win64-pyinstaller.zip .\dist\ESMB
+7z a -tzip -mx9 -y ESMB-%PLATFORM%-pyinstaller.zip .\dist\ESMB
 rd /S /Q dist
 
 C:\Python37-x64\Scripts\pyinstaller -F -y -w ESMB.py
-cp dist\ESMB ESMB-win64-pyinstaller.exe
+cp dist\ESMB ESMB-%PLATFORM%-pyinstaller.exe
 rd /S /Q dist
--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,18 +1,20 @@
-# Dependencies
+# Setup
+PLATFORM=$(uname -i)
+echo Running on $PLATFORM
 sudo apt-get update && sudo apt-get -y install python3-pip
 pip3 install nuitka pyinstaller
 
 # Nuitka Compilation
 python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
 mv ESMB.dist ESMB
-tar -czvf ESMB-amd64-nuitka.tar.gz ESMB/
+tar -czvf ESMB-ubuntu-$PLATFORM-nuitka.tar.gz ESMB/
 rm -rf ESMB
 
 # PyInstaller
 pyinstaller -D -y -w ESMB.py
-tar -czvf ESMB-amd64-pyinstaller.tar.gz dist/ESMB/
+tar -czvf ESMB-ubuntu-$PLATFORM-pyinstaller.tar.gz dist/ESMB/
 rm -rf dist
 
 pyinstaller -F -y -w ESMB.py
-cp dist/ESMB ESMB-amd64-pyinstaller
+cp dist/ESMB ESMB-ubuntu-$PLATFORM-pyinstaller
 rm -rf dist
