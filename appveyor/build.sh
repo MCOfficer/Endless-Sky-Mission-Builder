1a298423f0ceb13373bc67976058fc95bc8a8897
M*C*O <mcofficer@gmx.de>
2019-06-12 13:07:30 +0200

    fix tk bundling on linux

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,11 +1,11 @@
 # Setup
 PLATFORM=$(uname -i)
 echo Running on $PLATFORM
-sudo apt-get update && sudo apt-get -y install python3-pip
+sudo apt-get update && sudo apt-get -y install python3-pip python3-tk
 pip3 install nuitka pyinstaller
 
 # Nuitka Compilation
-python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons --plugin-enable=tk-inter ESMB.py
+python3 -m nuitka --assume-yes-for-downloads --standalone --show-progress --show-scons ESMB.py
 mv ESMB.dist ESMB
 tar -czvf ESMB-ubuntu-$PLATFORM-nuitka.tar.gz ESMB/
 rm -rf ESMB
